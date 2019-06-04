44be23c997da1ba1690f0ea4dc76ff26c3526e35
t-kusakabe <ttmac0704@gmail.com>
2019-06-05 07:17:58 +0900

    Use camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
         title: 'Flutter Demo',
         theme: new ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.red,
         ),
         home: new MyHomePage(),
     );
@@ -25,11 +26,51 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-            title: new Text("ML Kit Sample"),
+            title: new Text("文字読み取り"),
         ),
-        body: new Center(
-            child: Text("Hello World"),
+        body: SingleChildScorollView(
+            child: Column(
+                children: <Widget>[
+                  _startCamera(),
+                ],
+            ),
         ),
     );
+  }
+
+  Widget _startCamera() {
+    return Contianer(
+        margin: EdgeInsets.only(top: 10.0),
+        child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        splashColor: Colors.blueGrey,
+                        onPressed: () {
+                          _onPickImageSelected();
+                        },
+                        child: Text('start camera'),
+                    ),
+                  ),
+              ),
+            ],
+        ),
+    );
+  }
+
+  void _onPickImageSelected() async {
+    var imageSource = Images
+  }
+
+  try {
+    final file = await ImagePicker.pickImage(source: imageSource);
+
+    if (file == null) {
+      throw Exception('ファイルを取得できませんでしたか')
+    }
   }
 }
